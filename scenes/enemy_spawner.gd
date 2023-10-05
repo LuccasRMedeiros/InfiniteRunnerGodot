5b4196a764cfe68b62584f0bd3f02d442d5bd6b1
Luccas Rocigno <luccasrocigno@hotmail.com>
2023-10-05 00:30:38 -0300

    Recreated enemies with Area2D node type

--- scenes/enemy_spawner.gd
+++ scenes/enemy_spawner.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
-var spawn_enemy = true
+const ENEMY_SPAWN_CHANCE = 1.0 / 2.0
+var ENEMY_SPAWN_TRIGGER: float
 
 func _ready():
-	pass
+	# Grants that ENEMY_SPAWN_TRIGGER is always initialized to 0
+	ENEMY_SPAWN_TRIGGER = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
-	if spawn_enemy:	
-		spawn_enemy = false
-		await get_tree().create_timer(2.0, true, false, false).timeout
+	ENEMY_SPAWN_TRIGGER += ENEMY_SPAWN_CHANCE * delta
 
-		print("Should spawn enemy now")
+	if ENEMY_SPAWN_TRIGGER >= 1.0:	
+		ENEMY_SPAWN_TRIGGER = 0.0
 
 		var enemy = preload("res://actors/BasicEnemy.tscn").instantiate()
-		enemy.position.x = 1128
-		enemy.position.y = 16
+		enemy.position.x = 1216
+		enemy.position.y = 440
+		enemy.mov_speed = 300.0
 
 		add_child(enemy)
--- scripts/Enemy.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-const SPEED = 300.0
-
-func _physics_process(delta):
-	
-	velocity.x = SPEED  * -1;
-
-	move_and_slide()
